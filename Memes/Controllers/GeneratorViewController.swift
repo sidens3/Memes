5769d2379991750ab42b185fc324beb9bc6bd9a4
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-11-27 14:49:43 +0300

    add launch screen views

---
+++ Memes/Controllers/GeneratorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GeneratorViewController.swift
+//  Memes
+//
+//  Created by Михаил Зиновьев on 27.11.2021.
+//
+
+import UIKit
+
+class GeneratorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
