60f39367cb16530238c0fcb9bd6fb9fc6bf09c3d
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-11-27 15:10:54 +0300

    configure UI on GeneratorViewController

--- Memes/Controllers/GeneratorViewController.swift
+++ Memes/Controllers/GeneratorViewController.swift
@@ -12,18 +12,35 @@ class GeneratorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        testApi()
+    }
+}
+
+//MARK: - Private
+private extension GeneratorViewController {
+    func testApi() {
+        let headers = [
+            "x-rapidapi-host": "ronreiter-meme-generator.p.rapidapi.com",
+            "x-rapidapi-key": "b47c20d26fmsh9a638b90d3c84fcp1de599jsn6eae9cde9d97"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://ronreiter-meme-generator.p.rapidapi.com/meme?top=Top%20Text&bottom=Bottom%20Text&meme=Condescending-Wonka&font_size=50&font=Impact")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        })
+
+        dataTask.resume()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Memes/Models/GeneratorManager.swift
@@ -0,0 +1,8 @@
+//
+//  GeneratorManager.swift
+//  Memes
+//
+//  Created by Михаил Зиновьев on 27.11.2021.
+//
+
+import Foundation
